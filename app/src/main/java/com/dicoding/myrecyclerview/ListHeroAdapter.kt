5a15489e7d1100f1f83f735e7d93ffddaba73377
MuhammadBobby <m.bobbyoktaviano@gmail.com>
2024-09-25 10:36:57 +0700

    Menggunakan menu dan menambah tampilan grid sebagai option

--- app/src/main/java/com/dicoding/myrecyclerview/ListHeroAdapter.kt
+++ app/src/main/java/com/dicoding/myrecyclerview/ListHeroAdapter.kt
@@ -5,10 +5,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import java.lang.reflect.Array
 
 class ListHeroAdapter (private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder> () {
+    private  lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback (onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
         val tvName:TextView = itemView.findViewById(R.id.tv_item_name)
@@ -27,6 +34,16 @@ class ListHeroAdapter (private val listHero: ArrayList<Hero>) : RecyclerView.Ada
         holder.imgPhoto.setImageResource(photo)
         holder.tvName.text = name
         holder.tvDescription.text = description
+
+        //set untuk setiap item di klik
+//        holder.itemView.setOnClickListener {
+//            Toast.makeText(holder.itemView.context, "Kamu memilih " + listHero[holder.adapterPosition].name, Toast.LENGTH_SHORT).show()
+//        }
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listHero[holder.adapterPosition]) }
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Hero)
     }
 
 }
--- app/src/main/java/com/dicoding/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/dicoding/myrecyclerview/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.dicoding.myrecyclerview
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -24,6 +28,26 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+//    menambah fungsi menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+//    menentukan ketika menu dipilih
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_list -> {
+                rvHeroes.layoutManager = LinearLayoutManager(this)
+            }
+            R.id.action_grid -> {
+                rvHeroes.layoutManager = GridLayoutManager(this, 2)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
     private fun getListHero(): ArrayList<Hero> {
         val dataName = resources.getStringArray(R.array.data_name)
         val dataDesctiption = resources.getStringArray(R.array.data_description)
@@ -38,9 +62,21 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    //    menandai item yang dipilih
+    private fun showSelectedHero(hero: Hero) {
+        Toast.makeText(this, "Kamu memilih " + hero.name, Toast.LENGTH_SHORT).show()
+    }
+
+
     private fun showRecyclerList() {
         rvHeroes.layoutManager = LinearLayoutManager(this)
         var listHeroAdapter = ListHeroAdapter(list)
         rvHeroes.adapter = listHeroAdapter
+
+        listHeroAdapter.setOnItemClickCallback(object : ListHeroAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Hero) {
+                showSelectedHero(data)
+            }
+        })
     }
 }
